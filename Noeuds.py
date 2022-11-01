1af3f47304717801509da0e40422e29251ae9842
Roude JEAN MARIE <jm_roude@outlook.fr>
2022-11-01 21:50:27 +0100

    Vertex et Edge presque complet

--- Noeuds.py
+++ Noeuds.py
@@ -2,49 +2,122 @@
 
 import numpy as np
 import pandas as pd
+import igraph as ig
 from biopandas.pdb import PandasPdb
 
 # Objets BioPandas
 ppdb1 = PandasPdb()
 ppdb2 = PandasPdb()
 
-# Lecture des fichiers pdb
-ppdb1.read_pdb('PDB/PDB_Binding/1AWR_P-A.pdb')
-ppdb2.read_pdb('PDB/1BXB.pdb')
+# Lecture des fichiers pdb sous forme de DataFrame
+ppdb1.read_pdb('PDB/2ptn.pdb')
+ppdb2.read_pdb('PDB/1AWR.pdb')
 
 records = ['ATOM', 'HETATM'] # records à conserver dans le dataframe
 columns_to_keep = ['element_symbol'] # Colonnes à conserver
 columns_to_inner = ['element_symbol'] # Colonne à utiliser pour la jointure
 
-# DataFrame 1 et 2 contenant les informations des sections section des
-#   fichiers pdb
+# DataFrame 1 et 2 avec les sections nécessaires (ATOM/ HETATM)
 ppdb_df1 = pd.concat([ppdb1.df[section] for section in records])
 ppdb_df2 = pd.concat([ppdb2.df[section] for section in records])
 
-ppdb_df1_to_keep = ppdb_df1[columns_to_keep][:5] # Dtf avec colonnes d'intérêts
-ppdb_df2_to_keep = ppdb_df2[columns_to_keep][:5] # Dtf avec colonnes d'intérêts
+# Dtf avec colonnes d'intérêts index, et columns_to_keep
+ppdb_df1_to_keep = ppdb_df1[columns_to_keep].reset_index(level=0)
+ppdb_df2_to_keep = ppdb_df2[columns_to_keep].reset_index(level=0)
 
-ppdb_df1_to_keep = ppdb_df1_to_keep.reset_index(level=0) # Ajout de la colonne indice
-ppdb_df2_to_keep = ppdb_df2_to_keep.reset_index(level=0) # Ajout de la colonne indice
+# X sera le dataframe avec le plus grand nombre d'atomes
+X, Y = None, None
+X_dtf, Y_dtf = None, None
+
+if ppdb_df1_to_keep.shape[0] >= ppdb_df2_to_keep.shape[0]:
+    X = ppdb_df1_to_keep
+    Y = ppdb_df2_to_keep
+    X_dtf = ppdb_df1
+    Y_dtf = ppdb_df2
+else:
+    X = ppdb_df2_to_keep
+    Y = ppdb_df1_to_keep
+    X_dtf = ppdb_df2
+    Y_dtf = ppdb_df1
+
+# Tailles N et M respectivement des dataframes X et Y
+N = X.shape[0] # Nombre de lignes dans X
+M = Y.shape[0] # Nombre de lignes dans Y
 
 # Jointure entre les éléments du fichier PDB1 et PDB2
-df_inner = ppdb_df1_to_keep.merge(ppdb_df2_to_keep, on=columns_to_inner, how='inner')
+#   jointure sur les colonnes dans columns_to_inner
+df_inner = X.merge(Y, on=columns_to_inner, how='inner')
+X_idx = df_inner["index_x"]
+Y_idx = df_inner["index_y"]
 
-# Liste des indices présents dans la jointure entre PDB1 et PDB2
-atm_index1 = df_inner["index_x"].unique() # Liste des indices chez PDB1
-atm_index2 = df_inner["index_y"].unique() # Liste des indices chez PDB2
+vertex = np.c_[X_idx, Y_idx] #?
 
-vertex = np.c_[df_inner["index_x"].T, df_inner["index_y"].T]
-edge_list = []
-edge = 0
+# Calcul la distance entre 2 atomes à partir des
+def distance_with_df(atm_idx1, atm_idx2, ppdb_df1, ppdb_df2):
+    """
+    Calcul la distance entre 2 atomes à partir
+      des coordonnées x, y, z issus des dataframes
 
-def distance(atm_idx1, atm_idx2, ppdb_df1, ppdb_df2, coord_names = None):
-    if coord_names == None:
-        coord_names = ['x_coord', 'y_coord', 'z_coord']
+    Arguments :
+        atm_idx1 : indice de l'atome 1
+        atm_idx2 : indice de l'atome2
+        ppdb_df1 : dataframe 1
+        ppdb_df2 : dataframe 2
+    """
+    
+    coord_names = ['x_coord', 'y_coord', 'z_coord']
     x = ppdb_df1[coord_names].iloc[[atm_idx1]].values
     y = ppdb_df2[coord_names].iloc[[atm_idx2]].values
     return(np.linalg.norm(x - y))
 
-print(atm_index1)
-print(atm_index2)
-print(df_inner.head(20))
+
+# Edges
+# N > M
+# n inclus dans [0, N-1]; m inclus dans [0,M-1]
+# i = 0
+# for x_idx, y_idx in vertex:
+#    dist_idx = distance_with_df(x_idx, y_idx, X_dtf, Y_dtf)
+#    i += 1
+#    for x_idx_prm, y_idx_prm in vertex[i:]:
+#        dist_idx_prm = distance_with_df(x_idx_prm, y_idx_prm, X_dtf, Y_dtf)
+#        md_idx = x_idx*N + y_idx
+#        md_idx_prm = x_idx_prm*N + y_idx_prm
+#
+#        #print(f"(i={x_idx:.2f}, j={y_idx:.2f}) - (i'={x_idx_prm:.2f}, j'={y_idx_prm:.2f})'")
+#        #print(f"md={md_idx:.2f} - md'={md_idx_prm:.2f}")
+#
+#        #print(f"i={md_idx//N} j={md_idx%N}")
+#        #print(f"i'={md_idx_prm//N} j'={md_idx_prm%N}\n")
+#        if(abs(dist_idx-dist_idx_prm) < seuil):
+#            edge_list.append((md_idx, md_idx_prm))
+#    
+# print(edge_list)
+
+# {Numéro indice: position dans le vertex}
+atm_idx_pos =  {k: v[0] for k,v in X_idx.groupby(X_idx, sort=False).groups.items()}
+atm_idx_keys = list(atm_idx_pos) #key
+
+seuil = 0.1
+
+edges = []
+
+i = 0
+j = 0
+for x_idx, y_idx in vertex:
+    key_idx = atm_idx_keys.index(x_idx) + 1
+    if(key_idx >= len(atm_idx_keys)):
+        break
+
+    j = atm_idx_pos[atm_idx_keys[key_idx]]
+    
+    for x_idx_prm, y_idx_prm in vertex[j:]:
+        dist_idx = distance_with_df(x_idx, x_idx_prm, X_dtf, Y_dtf)
+        dist_idx_prm = distance_with_df(y_idx, y_idx_prm, X_dtf, Y_dtf)
+
+        if(abs(dist_idx-dist_idx_prm) < seuil):
+            edges.append((i, j))
+        j += 1
+    i += 1
+
+
+n_vertice = vertex.shape[0]
