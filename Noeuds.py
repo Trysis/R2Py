c2f7d9a32259a7fa22a4f4d9cae57c76afdc51bd
Roude JEAN MARIE <jm_roude@outlook.fr>
2022-11-18 00:38:07 +0100

    Edge indices

--- Noeuds.py
+++ Noeuds.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from ctypes import *
+import igraph as ig
 import numpy as np
 import pandas as pd
 from biopandas.pdb import PandasPdb
@@ -13,12 +14,12 @@ ppdb1.read_pdb('PDB/2ptn.pdb')
 ppdb2.read_pdb('PDB/1AWR.pdb')
 
 records = ['ATOM', 'HETATM'] # records à conserver dans le dataframe
-columns_to_keep = ['element_symbol'] # Colonnes à conserver
+columns_to_keep = ["x_coord", "y_coord", "z_coord", 'element_symbol'] # Colonnes à conserver
 columns_to_inner = ['element_symbol'] # Colonne à utiliser pour la jointure
 
 # DataFrame 1 et 2 avec les sections nécessaires (ATOM/ HETATM)
-ppdb_df1 = pd.concat([ppdb1.df[section] for section in records])[:6]
-ppdb_df2 = pd.concat([ppdb2.df[section] for section in records])[:4]
+ppdb_df1 = pd.concat([ppdb1.df[section] for section in records])
+ppdb_df2 = pd.concat([ppdb2.df[section] for section in records])
 
 # Dtf avec colonnes d'intérêts index, et columns_to_keep
 ppdb_df1_to_keep = ppdb_df1[columns_to_keep].reset_index(level=0)
@@ -52,19 +53,20 @@ X_idx = df_inner["index_x"]
 Y_idx = df_inner["index_y"]
 
 vertex = np.c_[X_idx, Y_idx] #?
-distX = X_dtf.loc[X_idx, ["x_coord", "y_coord", "z_coord"]]
-distY = Y_dtf.loc[Y_idx, ["x_coord", "y_coord", "z_coord"]]
+distX = df_inner[["x_coord_x", "y_coord_x", "z_coord_x"]]
+distY = df_inner[["x_coord_y", "y_coord_y", "z_coord_y"]]
 
 func_edge = CDLL('D:/JM_Roude/Master_BioInformatique-Ingenieurie-de-Plateforme/UEs/Stage 2/R2Py/edgex.so')
 func_edge.edge.restype = POINTER(POINTER(c_int))
 # X et Y
 n_col = (distX.shape[1])
+n_row = (distX.shape[0])
 
 pp_doubleX = POINTER(POINTER(c_double))
 pp_doubleY = POINTER(POINTER(c_double))
 
-process_distX = [cast((c_double * distX.shape[0])(*distX[col_in_X].values), POINTER(c_double)) for col_in_X in distX]
-process_distY = [cast((c_double * distY.shape[0])(*distY[col_in_Y].values), POINTER(c_double)) for col_in_Y in distY]
+process_distX = [cast((c_double * n_row)(*distX[col_in_X].values), POINTER(c_double)) for col_in_X in distX]
+process_distY = [cast((c_double * n_row)(*distY[col_in_Y].values), POINTER(c_double)) for col_in_Y in distY]
 
 X_pointer_val=(POINTER(c_double) * n_col)(*process_distX)
 Y_pointer_val=(POINTER(c_double) * n_col)(*process_distY)
@@ -72,67 +74,15 @@ Y_pointer_val=(POINTER(c_double) * n_col)(*process_distY)
 dist_pointerX = cast(X_pointer_val, pp_doubleX) #
 dist_pointerY = cast(Y_pointer_val, pp_doubleY) #
 
-X_idx_pointer = (c_int * distX.shape[0])(*X_idx.values)
-Y_idx_pointer = (c_int * distY.shape[0])(*Y_idx.values)
+X_idx_pointer = (c_int * n_row)(*X_idx.values)
+Y_idx_pointer = (c_int * n_row)(*Y_idx.values)
 
-print(distX)
-print(distY)
 nrow_edge = c_int()
 edge_mat = POINTER(POINTER(c_int))
 edge_mat.contents = func_edge.edge(byref(X_idx_pointer), byref(Y_idx_pointer),
                                     dist_pointerX, dist_pointerY,
                                     I, N, M, byref(nrow_edge))
 
-print(nrow_edge.value)
-print(f"{edge_mat.contents[0][30]} {edge_mat.contents[1][30]}")
 edge = [(edge_mat.contents[0][i], edge_mat.contents[1][i]) for i in range(nrow_edge.value)]
+print(edge)
 exit()
-
-# Edges
-# N > M
-# n inclus dans [0, N-1]; m inclus dans [0,M-1]
-# i = 0
-# for x_idx, y_idx in vertex:
-#    dist_idx = distance_with_df(x_idx, y_idx, X_dtf, Y_dtf)
-#    i += 1
-#    for x_idx_prm, y_idx_prm in vertex[i:]:
-#        dist_idx_prm = distance_with_df(x_idx_prm, y_idx_prm, X_dtf, Y_dtf)
-#        md_idx = x_idx*N + y_idx
-#        md_idx_prm = x_idx_prm*N + y_idx_prm
-#
-#        #print(f"(i={x_idx:.2f}, j={y_idx:.2f}) - (i'={x_idx_prm:.2f}, j'={y_idx_prm:.2f})'")
-#        #print(f"md={md_idx:.2f} - md'={md_idx_prm:.2f}")
-#
-#        #print(f"i={md_idx//N} j={md_idx%N}")
-#        #print(f"i'={md_idx_prm//N} j'={md_idx_prm%N}\n")
-#        if(abs(dist_idx-dist_idx_prm) < seuil):
-#            edge_list.append((md_idx, md_idx_prm))
-#    
-# print(edge_list)
-
-# {Numéro indice: position dans le vertex}
-atm_idx_pos =  {k: v[0] for k,v in X_idx.groupby(X_idx, sort=False).groups.items()}
-atm_idx_keys = list(atm_idx_pos) #key
-
-seuil = 0.1
-
-edges = []
-
-i = 0
-j = 0
-for x_idx, y_idx in vertex:
-    key_idx = atm_idx_keys.index(x_idx) + 1
-    if(key_idx >= len(atm_idx_keys)):
-        break
-
-    j = atm_idx_pos[atm_idx_keys[key_idx]]
-    
-    for x_idx_prm, y_idx_prm in vertex[j:]:
-        dist_idx = distance_with_df(x_idx, x_idx_prm, X_dtf, X_dtf)
-        dist_idx_prm = distance_with_df(y_idx, y_idx_prm, Y_dtf, Y_dtf)
-
-        if(abs(dist_idx-dist_idx_prm) < seuil):
-            edges.append((i, j))
-        j += 1
-    i += 1
-
--- edgex.c
+++ edgex.c
@@ -12,22 +12,26 @@ double dist3D(double x1, double y1, double z1, double x2, double y2, double z2){
     return sqrt(dx*dx + dy*dy + dz*dz);
 }
 
-int ** edge(int * index_X, int * index_Y, double ** coordX, double ** coordY, int nrow_coord, int N, int M, int * nrow_edge){
-    int ** edge_matrix = malloc(sizeof(int*) * 2); // edge matrix
+int ** edge(int * index_X, int * index_Y, double ** coordX, double ** coordY, int nrow_coord, int N, int M, size_t * nrow_edge){
+    int ** edge_matrix = malloc(sizeof(int *) * 2); // edge matrix
     assert(edge_matrix != NULL);
 
+    size_t size = (size_t)(nrow_coord * nrow_coord);
+    
     //Allocation mémoire de chaques colonnes
     for(int i=0; i<2; i++){
-        edge_matrix[i] = malloc(sizeof(int) * nrow_coord * nrow_coord);
+        edge_matrix[i] = malloc(sizeof(int) * size);
         assert(edge_matrix[i] != NULL);
     }
 
     int seuil = 1; // Seuil en Angstrom
     *nrow_edge = 0;
     for(int i=0; i<nrow_coord; i++){
+        printf("%d\n", *nrow_edge);
         for(int j=i+1; j<nrow_coord; j++){
+            if(*nrow_edge == (size_t) 48043500)printf("j=%d\n", j);
             if(index_X[i] == index_X[j])continue;
-            if(*nrow_edge == index_Y[j])continue;
+            if(index_Y[i] == index_Y[j])continue;
 
             // coordonnee dans X
             double x1 = coordX[0][i];
@@ -50,10 +54,12 @@ int ** edge(int * index_X, int * index_Y, double ** coordX, double ** coordY, in
             //Calcul des distances
             double d1 = dist3D(x1, y1, z1, x1_prm, y1_prm, z1_prm);
             double d2 = dist3D(x2, y2, z2, x2_prm, y2_prm, z2_prm);
+            
             if(abs(d1 - d2) <= seuil){
+                /*
                 int md_idx = index_X[i]*N + index_Y[i];
                 int md_idx_prm = index_X[j]*N + index_Y[j];
-
+                
                 printf("N = %d; M = %d\n", N, M);
                 printf("i:%d [%f, %f, %f]; j:%d [%f, %f, %f]\n",
                     index_X[i], x1, y1, z1,
@@ -64,17 +70,26 @@ int ** edge(int * index_X, int * index_Y, double ** coordX, double ** coordY, in
                 
                 printf("md_idx = %d; md_idx_prm = %d\n", md_idx, md_idx_prm);
                 printf("d1 = %f; d2 = %f\n\n", d1, d2);
+                */
 
-                edge_matrix[0][*nrow_edge] = md_idx;
-                edge_matrix[1][*nrow_edge] = md_idx_prm;
+                edge_matrix[0][*nrow_edge] = i;
+                edge_matrix[1][*nrow_edge] = j;
 
-                *nrow_edge += 1;
+                *nrow_edge += (size_t) 1;
+                if(*nrow_edge >= size){
+                    size += (size_t)(nrow_coord * nrow_coord);
+                    for(int i=0; i<2; i++){
+                            (int *) realloc(edge_matrix[i], sizeof(int) * size);
+                            assert(edge_matrix[i] != NULL);
+                    }
+                }
             }
         }
     }
+    printf("hey");
     // Reallocation de la memoire au nombre de lignes attribuee
     for(int i=0; i<2; i++){
-        (int *) realloc(edge_matrix[i], sizeof(int) * (*nrow_edge));
+        (int *) realloc(edge_matrix[i], sizeof(int) * (size_t)(*nrow_edge));
         assert(edge_matrix[i] != NULL);
     }
     return edge_matrix;
